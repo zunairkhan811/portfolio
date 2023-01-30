9ca4ec5f7fb89596c3f609934a8c9eabcc367bfa
zunairkhan811 <zunairkhan811@gmail.com>
2023-01-31 00:52:30 +0500

    Linter error removed

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+// eslint-disable-next-line no-unused-vars
 function onClickMenu() {
-    document.getElementById("logo").classList.toggle("change-leftnavbar-logo")
-    document.getElementById("wrap").classList.toggle("change-wrap")
-    document.getElementById("right-navbar").classList.toggle("change-rightnavbar")
+  document.getElementById('logo').classList.toggle('change-leftnavbar-logo');
+  document.getElementById('wrap').classList.toggle('change-wrap');
+  document.getElementById('right-navbar').classList.toggle('change-rightnavbar');
 }
