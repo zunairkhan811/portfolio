8999b173dbbbd669f68a0c33ea73bdf615cf6add
zunairkhan811 <zunairkhan811@gmail.com>
2023-01-31 11:13:22 +0500

    Make changes

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-// eslint-disable-next-line no-unused-vars
-function onClickMenu() {
-  document.getElementById('logo').classList.toggle('change-leftnavbar-logo');
-  document.getElementById('wrap').classList.toggle('change-wrap');
-  document.getElementById('right-navbar').classList.toggle('change-rightnavbar');
-}
+const logoId = document.querySelector('#logo');
+const wrapId = document.querySelector('#wrap');
+const rightNavebarId = document.querySelector('#right-navbar');
+const portfolioId = document.querySelector('#portfolioicon');
+const aboutId = document.querySelector('#abouticon');
+const contactId = document.querySelector('#contacticon');
+
+wrapId.addEventListener('click', () => {
+  wrapId.classList.toggle('change-wrap');
+  logoId.classList.toggle('change-leftnavbar-logo');
+  rightNavebarId.classList.toggle('change-rightnavbar');
+});
+portfolioId.addEventListener('click', () => {
+  wrapId.classList.remove('change-wrap');
+  logoId.classList.remove('change-leftnavbar-logo');
+  rightNavebarId.classList.remove('change-rightnavbar');
+});
+aboutId.addEventListener('click', () => {
+  wrapId.classList.remove('change-wrap');
+  logoId.classList.remove('change-leftnavbar-logo');
+  rightNavebarId.classList.remove('change-rightnavbar');
+});
+contactId.addEventListener('click', () => {
+  wrapId.classList.remove('change-wrap');
+  logoId.classList.remove('change-leftnavbar-logo');
+  rightNavebarId.classList.remove('change-rightnavbar');
+});
